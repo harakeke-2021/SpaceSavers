6d97f6cc4da8203d8fe18c319f5a52cac872ca15
Peter <peter.lim91@gmail.com>
2021-02-25 11:27:09 +1300

    put key in env

--- server/routes/parker.js
+++ server/routes/parker.js
@@ -1,7 +1,9 @@
+const path = require('path')
 const express = require('express')
 
 const router = express.Router()
 const { Client } = require('@googlemaps/google-maps-services-js')
+require('dotenv').config()
 
 module.exports = router
 
@@ -11,12 +13,13 @@ router.get('/', (req, res) => {
     .geocode({
       params: {
         address: '17a walter street, auckland',
-        key: 'AIzaSyAwonXg89LWspEiD10wgptbWOuK8lLh6VI'
+        key: process.env.GEOCODE_API_KEY
       },
       timeout: 1000 // milliseconds
     })
     .then((r) => {
       console.log(r.data.results[0].geometry)
+      return null
     })
     .catch((e) => {
       console.log(e.response.data.error_message)
