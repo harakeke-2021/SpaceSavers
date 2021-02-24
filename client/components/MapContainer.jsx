aae4d8ca5fb5f6cd45b6186d53bce8b468d1a216
Anna McCormack <anna.mccormack94@gmail.com>
2021-02-25 11:48:13 +1300

    add api key

--- client/components/MapContainer.jsx
+++ client/components/MapContainer.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import GoogleMapReact from 'google-map-react'
+import GoogleMapReact from 'google-maps-react'
 import MapMarker from './MapMarker'
 
-function MapContainer(props) {
-  const key = PUTKEYHERE
+function MapContainer (props) {
+  const key = AIzaSyAwonXg89LWspEiD10wgptbWOuK8lLh6VI
 
   const center = {
     lat: -36.84978,
     lng: 174.7586
   }
 
-  function handleApiLoaded(map, maps) {
+  function handleApiLoaded (map, maps) {
     console.log('map', map)
     console.log('maps', maps)
     const options = { gestureHandling: 'none' }
