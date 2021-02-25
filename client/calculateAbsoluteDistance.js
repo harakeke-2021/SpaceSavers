e05a9e027fbfc37747153a5173a74b17112b2c3b
clinton <cphilathong1@gmail.com>
2021-02-25 22:38:23 +1300

    implement absolute distance between two coords

--- client/calculateAbsoluteDistance.js
+++ client/calculateAbsoluteDistance.js
@@ -2,7 +2,6 @@ const { log10Dependencies, atan2 } = require('mathjs')
 const math = require('mathjs')
 
 const test = math.sin(math.unit(90, 'deg'))
-console.log(test)
 
 const R = 6373
 
@@ -14,7 +13,7 @@ const lat2 = 174.7760
 const dlon = lon1 - lon2
 const dlat = lat1 - lat2
 
-const a = math.square(math.sin(dlat / 2)) + (math.cos(lat2) * math.cos(lat2) * math.square(math.sin(dlon / 2)))
+const a = math.square(math.sin(math.unit(dlat / 2, 'deg'))) + (math.cos(math.unit(lat2, 'deg')) * math.cos(math.unit(lat2, 'deg')) * math.square(math.sin(math.unit(dlon / 2, 'deg'))))
 const c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
 const d = R * c
