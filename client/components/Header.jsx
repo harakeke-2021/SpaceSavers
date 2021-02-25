9f017e943934962e693bf3ec9ca8530d416d08eb
Anna McCormack <anna.mccormack94@gmail.com>
2021-02-25 15:07:19 +1300

    add header component

---
+++ client/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import NavBar from './NavBar'
+
+function Header () {
+  return (
+    <header>
+      <h1>Space Saver</h1>
+      <NavBar />
+    </header>
+  )
+}
+
+export default Header
