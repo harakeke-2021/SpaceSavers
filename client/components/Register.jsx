6137822267cd2b566a8410cb5af568dc921f6f38
Pete Angjelevski <p.angjelevski@gmail.com>
2021-02-26 14:58:08 +1300

    add register component

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,14 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { register, isAuthenticated } from 'authenticare/client'
 
 export default function Register (props) {
+  const [error, setError] = useState('')
+  const [form, setForm] = useState({
+    username: '',
+    password: ''
+  })
+
+  const hideError = () => {
+    setError('')
+  }
+
+  function handleChange (e) {
+    const { name, value } = e.target
+    setForm({
+      ...form,
+      [name]: value
+    })
+  }
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    const { username, password } = form
+    register({ username, password })
+      .then(() => {
+        if (isAuthenticated()) {
+          props.history.push('/')
+        }
+        return null
+      })
+      .catch(err => {
+        if (err.message === 'USERNAME_UNAVAILABLE') {
+          setError('Username is not available')
+        }
+      })
+  }
+
   return (
-    <form>
-        <label>Username</label>
-        <input/>
+    <>
+        <div style={{'color: red; cursor: pointer;'}} onClick={hideError}>
+        { error && `Error: ${error}` }
+        </div>
+      <form>
+        <label>Choose a Username:</label>
+        <input type="text" onChange={handleChange}/>
 
-        <label>Password</label>
-        <input /> 
+        <label>Choose a Password:</label>
+        <input type="text" onChange={handleChange}/>
 
-        <button>Register</button>
-    </form>
+        <button onSubmit={handleSubmit}>Register</button>
+      </form>
+    </>
+  )
 }
