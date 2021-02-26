8513eb96179c7d33c0fdfd0e51676281636a0a41
Pete Angjelevski <p.angjelevski@gmail.com>
2021-02-27 08:58:21 +1300

    add navBar logOff

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { logOff } from 'authenticare/client'
+
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 
 function NavBar () {
   return (
     <div>
-      <Link to='/login'>Log in </Link>
-      <Link to='/register'>Register </Link>
+      <Link to='/'>Home</Link>
+      <IfAuthenticated>
+        <Link to='#' onClick={logOff}>Log Off</Link>
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <Link to='/register'>Register </Link>
+        <Link to='/signin'>Sign In</Link>
+      </IfNotAuthenticated>
     </div>
   )
 }
