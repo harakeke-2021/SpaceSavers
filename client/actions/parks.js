9fcda9c0eda6333298d134f6eafcd657d9cb7dea
clinton <cphilathong1@gmail.com>
2021-02-28 19:35:58 +1300

    add alias

--- client/actions/parks.js
+++ client/actions/parks.js
@@ -3,6 +3,7 @@ import { dispatch } from '../store'
 
 export const GET_ALL_PARKS = 'GET_ALL_PARKS'
 export const GET_PARKER_HISTORY = 'GET_PARKER_HISTORY'
+export const SET_ALL_PARKS = 'SET_ALL_PARKS'
 
 export function getAllParks (parks) {
   getAllParksApi()
@@ -17,8 +18,6 @@ export function getAllParks (parks) {
     .catch(err => console.error(err))
 }
 
-export function getHistoryByParkerId () {
-  // p
 export function setParks (parks) {
   return {
     type: SET_ALL_PARKS,
