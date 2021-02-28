14d80f605388f16264b2cb329f3cd6fdc156cd17
clinton <cphilathong1@gmail.com>
2021-03-01 11:43:18 +1300

    refactor

--- client/components/MapContainer.jsx
+++ client/components/MapContainer.jsx
@@ -72,8 +72,8 @@ function MapContainer (props) {
               key={park.lat}
               lat={park.lat}
               lng={park.lng}
-              price={park.price}
-              name={park.name}/> : null
+              obj={park}
+            /> : null
           )
         })}
       </GoogleMapReact>
