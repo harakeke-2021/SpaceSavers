12d8aefb735db6f575fe0b6664c9cbd0c0041d5b
Pete Angjelevski <p.angjelevski@gmail.com>
2021-03-01 16:42:15 +1300

    hide lng input newParkForm

--- client/components/NewParkForm.jsx
+++ client/components/NewParkForm.jsx
@@ -90,7 +90,7 @@ function NewParkForm (props) {
 
           <label name='lng'></label>
           <input
-            type="text"
+            type="hidden"
             value={geoCode.lng}
             key="lng"
           />
