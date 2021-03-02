f7dcc72ef4d81ab212b2ecadf1bdfe8f34314b2b
Anna McCormack <anna.mccormack94@gmail.com>
2021-03-02 21:22:44 +1300

    tyle history tables

--- client/components/OwnerHistory.jsx
+++ client/components/OwnerHistory.jsx
@@ -15,8 +15,8 @@ function OwnerHistory (props) {
       <div className='flex flex-col my-5 lg:w-3/4 m-auto'>
         <div className='my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
           <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
-            <div className='shadow overflow-hidden border border-blue-500 sm:rounded-lg'>
-              <table className='min-w-full divide-y divide-blue-500'>
+            <div className='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
+              <table className='min-w-full divide-y divide-gray-200'>
                 <thead className='bg-blue-500'>
                   <tr>
                     <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider'>
